366fa26031b0b9f13b8655927187264311627e49
Duy Mai <Duy.maid@one-line.com>
2024-03-19 14:56:17 +0700

    deploy dynamicQr

--- src/adapters/frameworks/express/routes/index.ts
+++ src/adapters/frameworks/express/routes/index.ts
@@ -1,69 +1,66 @@
-import {Express, json, Request, Response, Router, urlencoded} from 'express';
-import {IModuleRoute} from '../../../../shared/interfaces';
-import {SupabaseDataSource} from '../../database/supabase';
-import {locator} from '../../../../app.injector';
-import {AppSettingRoute} from './app-setting.route';
-import {AuthRoute} from './auth.route';
-import {EventsRoute} from './events.route';
+import { Express, json, Request, Response, Router, urlencoded } from 'express';
+import { IModuleRoute } from '../../../../shared/interfaces';
+import { locator } from '../../../../app.injector';
+import { AppSettingRoute } from './app-setting.route';
+import { AuthRoute } from './auth.route';
+import { EventsRoute } from './events.route';
 
 export class ModuleRoute implements IModuleRoute {
-    initialize(app: Express): void {
-        const router = Router();
+  initialize(app: Express): void {
+    const router = Router();
 
-        router.use(json());
-        router.use(urlencoded({extended: false}));
+    router.use(json());
+    router.use(urlencoded({ extended: false }));
 
-        this.load(router);
-        app.use('/v1', router);
+    this.load(router);
+    app.use('/v1', router);
 
+    app.get('/dynamic-qr', (_req, res) => {
+      let { amount } = _req.query;
+      let { txt } = _req.query;
+      let priceLength = amount.length;
+      let priceCode = '54' + this.formatStringLength(amount.toString()) + amount.toString();
+      let text = txt.toString().slice(0, -8) + priceCode + '6304'
+      res.json(text + this.calculateCRC(text).toString(16));
+    });
 
-        app.get('/dynamic-qr', (_req, res) => {
-            let { price } = _req.query;
-            let { txt } = _req.query;
-            let priceLength = price.length;
-            //00020101021138540010A00000072701240006970407011096020919960208QRIBFTTA53037045802VN830084005402126304C2CB
-            let priceCode = '54' + this.formatStringLength(price.toString()) + price.toString();
-            let text = txt.toString().slice(0, -8) + priceCode + '6304'
-            res.json(text + this.calculateCRC(text).toString(16));
-        });
+    app.get('/health', (_req: Request, res: Response) => {
+      res.json('OK');
+    });
+  }
 
-        app.get('/health', (_req: Request, res: Response) => {
-            res.json('OK');
-        });
-    }
+  load(router: Router): void {
+    router.use('/app', locator.get(AppSettingRoute).load());
+    router.use('/events', locator.get(EventsRoute).load());
+    router.use('/auth', locator.get(AuthRoute).load());
+  }
 
-    load(router: Router): void {
-        router.use('/app', locator.get(AppSettingRoute).load());
-        router.use('/events', locator.get(EventsRoute).load());
-        router.use('/auth', locator.get(AuthRoute).load());
-    }
+  calculateCRC(text: string): number {
+    let crc = 0xFFFF;
 
-    calculateCRC(text: string): number {
-        let crc = 0xFFFF;
+    for (let i = 0; i < text.length; i++) {
+      crc ^= text.charCodeAt(i) << 8;
 
-        for (let i = 0; i < text.length; i++) {
-            crc ^= text.charCodeAt(i) << 8;
-
-            for (let j = 0; j < 8; j++) {
-                if ((crc & 0x8000) !== 0) {
-                    crc = (crc << 1) ^ 0x1021;
-                } else {
-                    crc <<= 1;
-                }
-            }
-        }
-
-        return crc & 0xFFFF;
-    }
-
-    formatStringLength(str: string): string {
-        const length = str.length;
-        if (length < 10) {
-            return '0' + length.toString();
+      for (let j = 0; j < 8; j++) {
+        if ((crc & 0x8000) !== 0) {
+          crc = (crc << 1) ^ 0x1021;
         } else {
-            return length.toString();
+          crc <<= 1;
         }
+      }
     }
+
+    return crc & 0xFFFF;
+  }
+
+  formatStringLength(str: string): string {
+    const length = str.length;
+    if (length < 10) {
+      return '0' + length.toString();
+    } else {
+      return length.toString();
+    }
+  }
 
 }
 
